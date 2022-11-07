a2ff3993ceb9c756620bae358522abf433a6e750
shankarpandey7230 <shankarpandey7230@gmail.com>
2022-11-07 22:04:42 +1100

    more ways of asynthunk

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,5 +1,5 @@
 import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import axios from "axios";
 
 const url = 'https://course-api.com/react-useReducer-cart-project';
 
@@ -10,10 +10,23 @@ const initialState={
     isLoading: true,
 
 }
-export const getCartItems = createAsyncThunk('cart/getCartItems',  ()=>{
-    return fetch(url)
-    .then(resp => resp.json())
-    .catch((err)=>console.log(err));
+export const getCartItems = createAsyncThunk('cart/getCartItems', 
+async()=>{
+    try{
+        const resp = await axios(url);
+        
+        return resp.data;
+
+
+    }
+    catch(error){
+
+    }
+
+
+    // return fetch(url)
+    // .then(resp => resp.json())
+    // .catch((err)=>console.log(err));
     
     
 })
